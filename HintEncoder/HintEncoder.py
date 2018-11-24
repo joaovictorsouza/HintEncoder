710ac2b95b250de4d39378cefc5fc70947eb5f61
João Victor Rodrigues de Souza <joaovictor.rodsouza@gmail.com>
2018-11-24 21:56:06 -0200

    Adicionar arquivos de projeto.

---
+++ HintEncoder/HintEncoder.py
@@ -0,0 +1,77 @@
+import pandas as pd
+import numpy as np
+import re
+
+from dataset import Tokenizer
+
+from keras.models import Sequential
+from keras.layers.recurrent import LSTM, SimpleRNN
+from keras.layers.wrappers import TimeDistributed
+from keras.layers.core import Dense, RepeatVector, Activation, Dropout, Flatten
+from keras.preprocessing.sequence import pad_sequences
+from keras.layers.embeddings import Embedding
+
+def get_data(file_path):
+    data = pd.read_csv(file_path, encoding='latin1')
+    questions = data.Question
+    answers = data.Answer
+
+    return questions, answers
+
+def train():
+    questions, answers = get_data('train.csv')
+    question_maxlen = max(map(len, (x for x in questions)))
+    answer_maxlen = max(map(len, (x for x in answers)))
+    max_question_answer = max(question_maxlen, answer_maxlen)
+
+
+    tokenizer = Tokenizer("vocabulary.txt", max_question_answer)
+    vocab_size = tokenizer.vocab_size()
+
+    questions = tokenizer.normalize_text(questions)
+    answers = tokenizer.normalize_text(answers)
+    
+    x = np.zeros((len(questions), max_question_answer, vocab_size), dtype=np.bool)
+    y = np.zeros((len(answers), max_question_answer, vocab_size), dtype=np.bool)
+   
+    questions_t = questions.apply(tokenizer.tokenize_text)
+    answers_t = answers.apply(tokenizer.tokenize_text)
+
+
+    for i, sentence in enumerate(questions_t): 
+        x[i] = tokenizer.encode(sentence, max_question_answer, vocab_size)
+    for i, sentence in enumerate(answers_t):
+        y[i] = tokenizer.encode(sentence, max_question_answer, vocab_size)
+
+
+    hidden_size = 100
+    batch_size = 64
+    epochs = 15
+    print('Hidden Size / Batch size / Epochs = {}, {}, {}'.format(hidden_size, batch_size, epochs))
+
+    model = Sequential()
+    # "Encode" the input sequence using an RNN, producing an output of HIDDEN_SIZE
+    # note: in a situation where your input sequences have a variable length,
+    # use input_shape=(None, nb_feature).
+    model.add(LSTM(hidden_size, input_shape=(max_question_answer, vocab_size)))
+    # For the decoder's input, we repeat the encoded input for each time step
+    model.add(RepeatVector(max_question_answer))
+    # The decoder LSTM could be multiple layers stacked or a single layer
+    for _ in range(1):
+        model.add(LSTM(hidden_size, return_sequences=True))
+    
+    # For each of step of the output sequence, decide which character should be chosen
+    model.add(TimeDistributed(Dense(vocab_size)))
+    model.add(Activation('softmax'))
+    
+    model.compile(loss='categorical_crossentropy',
+                  optimizer='adam',
+                  metrics=['accuracy'])
+    
+    model.fit(x, y, batch_size=batch_size, epochs=epochs, validation_split=0.05)
+    model.save('my_model.h5')
+
+train()
+    
+
+
---
+++ HintEncoder/dataset.py
@@ -0,0 +1,84 @@
+import string
+import numpy as np
+
+class Tokenizer:
+    not_found_token = 'NF'
+
+    def __init__(self, vocab_path, max_seq_len):
+        self.vocab_path = vocab_path
+        self.max_seq_len = max_seq_len
+        self.word_to_index = {self.not_found_token : 0}
+        self.index_to_word = {0 : self.not_found_token}
+        self.__load_vocab(vocab_path)
+
+    def __load_vocab(self, vocab_path):
+        with open(vocab_path, encoding='utf-8') as f:
+            for line in f:
+                line = line[:-1]
+                self.add_vocab_word(line)
+
+    def remove_ponctuation(self, data):
+        table = str.maketrans({key: None for key in string.punctuation})
+        return data.translate(table)   
+    
+    def normalize_text(self, data):
+        for w in data:
+            w = self.remove_ponctuation(w.lower())
+        return data
+
+    def vocab_size(self):
+        return len(self.word_to_index)
+
+    def add_vocab_word(self, word):
+        idx = len(self.word_to_index)
+        self.word_to_index[word] = idx
+        self.index_to_word[idx] = word
+
+    def encode(self, sentence, maxlen, vocab_size):
+        X = np.zeros((maxlen, vocab_size))
+        for i, c in enumerate(sentence):
+            try:
+                X[i, self.word_to_index[c]] = 1
+            except Exception as e:
+                X[i, self.word_to_index[Tokenizer.not_found_token]] = 1
+        return X
+
+    def decode(self, X, calc_argmax=True):
+        if calc_argmax:
+            X = X.argmax(axis=-1)
+        return ''.join(self.index_to_word[x] for x in X)
+
+
+    def tokenize_text(self, paragraph):
+        paragraph = self.remove_ponctuation(paragraph)
+        results = []
+        for w in paragraph.split(' '):
+            results.append(w)
+        return results
+
+    def text_to_sequence(self, paragraph):
+        paragraph = self.remove_ponctuation(paragraph)
+        results = []
+        for w in paragraph.split(' '):
+            try:
+                idx = self.word_to_index[w]
+            except Exception as e:
+                idx = self.word_to_index[Tokenizer.not_found_token]
+                pass
+
+            results.append(idx)
+
+
+        # reverse and ensure we cut off or pad
+        results = results[::-1][:self.max_seq_len]
+        results.extend([0] * (self.max_seq_len - len(results)))
+        return np.asarray(results)
+
+    def texts_to_sequences(self, texts):
+        results = []
+        for text in texts:
+            result = self.text_to_sequence(text)
+            results.append(result)
+
+        return np.asarray(results)
+
